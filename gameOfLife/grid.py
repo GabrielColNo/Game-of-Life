fb020806699dd7e9253e3d2215f09005de0d3492
GabrielColNo <gabrielcolnotpro@gmail.com>
2025-11-13 11:06:40 +0100

    Init commit

---
+++ gameOfLife/grid.py
@@ -0,0 +1,47 @@
+class LifeGrid:
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def evolve(self):
+        pass
+
+    def as_string(self, bbox):
+        pass
+
+    def __str__(self):
+        pass
+
+import collections
+
+class LifeGrid:
+    # ...
+    def evolve(self):
+        neighbors = (
+            (-1, -1),  # Above left
+            (-1, 0),  # Above
+            (-1, 1),  # Above right
+            (0, -1),  # Left
+            (0, 1),  # Right
+            (1, -1),  # Below left
+            (1, 0),  # Below
+            (1, 1),  # Below right
+        )
+        num_neighbors = collections.defaultdict(int)
+        for row, col in self.pattern.alive_cells:
+            for drow, dcol in neighbors:
+                num_neighbors[(row + drow, col + dcol)] += 1
+
+        stay_alive = {
+            cell for cell, num in num_neighbors.items() if num in {2, 3}
+        } & self.pattern.alive_cells
+        come_alive = {
+            cell for cell, num in num_neighbors.items() if num == 3
+        } - self.pattern.alive_cells
+
+        self.pattern.alive_cells = stay_alive | come_alive
+
+        def __str__(self):
+            return (
+                f"{self.pattern.name}:\n"
+                f"Alive cells -> {sorted(self.pattern.alive_cells)}"
+            )
